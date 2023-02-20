defaa715232b7d6f04fd5e49bcbe89c1515355a0
tesddev <tesleem.amuda@gmail.com>
2023-02-20 09:31:15 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by GIGL iOS on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
