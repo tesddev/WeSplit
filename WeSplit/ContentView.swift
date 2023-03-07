00adb2749dc089d1a0a272e7955a4543ae367617
tesddev <tesleem.amuda@gmail.com>
2023-03-07 20:00:52 +0100

    add basic properties

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -2,20 +2,19 @@
 //  ContentView.swift
 //  WeSplit
 //
-//  Created by GIGL iOS on 20/02/2023.
+//  Created by Tes on 20/02/2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello World!")
     }
 }
 
