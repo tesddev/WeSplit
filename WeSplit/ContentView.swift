448139ece3545672f730e2828f99b1406a2d28f9
tesddev <tesleem.amuda@gmail.com>
2023-03-13 09:47:38 +0100

    add currency textfield

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,7 +14,11 @@ struct ContentView: View {
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Text("Hello World!")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+        }
     }
 }
 
