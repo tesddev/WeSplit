c712ffa85a175de29d483b4ecc0b3aacb3bd014e
tesddev <tesleem.amuda@gmail.com>
2023-04-13 15:47:09 +0100

    add header to third section

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -49,6 +49,8 @@ struct ContentView: View {
                 }
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Amount per person")
                 }
             }
             .navigationTitle("WeSplit")
